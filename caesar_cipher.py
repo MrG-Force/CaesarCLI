c0d5a08d89c432c52baff50f17d554c8e16ee5cc
MrGForce <willortizloza@hotmail.com>
2022-04-26 21:36:22 +1000

    add function docstring

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -6,7 +6,15 @@ def main():
 
 
 def cipher(message, shift, direction):
-    # prefix = "en" if direction == "encode" else "de"
+    """Return an encrypted or decrypted message using the given shift value as key
+        Parameters:
+            message (str): the text to decrypt or encrypt
+            shift (int): the amount of positions to shift each letter
+            direction (str): e for encode, shift forward, d for decode shift backwards
+
+        Returns:
+            result (str): the either encrypted or decrypted message
+    """
     d = 1 if direction == "e" else -1
     uppers = string.ascii_uppercase
     lowers = string.ascii_lowercase
